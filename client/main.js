a6ab709ef22da27556e588e0e72a2b26c35832be
albertbahia <albert.bahia1@gmail.com>
2015-04-12 15:51:08 -0400

    added server js code and removed autopublish package

--- client/main.js
+++ client/main.js
@@ -105,14 +105,3 @@ Meteor.methods({
     Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 });
-
-if (Meteor.isServer) {
-  Meteor.publish("tasks", function() {
-    return Tasks.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
-}
